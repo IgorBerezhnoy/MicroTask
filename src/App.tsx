fad26ce6d0926a4c3de240ecd9684feb01d9ab43
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2023-05-14 14:45:11 +0800

    added button hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
-import {Header} from './site/Header';
-import {Body} from './site/Body';
-import {Footer} from './site/Footer';
-import {NewComponent} from './NewComponent';
-import {NewComponent2} from './NewComponent2';
-import {Button} from './components/Button';
-
-const ButtonFoo = (name: string, age: number, address: string) => {
-    console.log('I am ' + name + ' ' + age + address);
-};
 
 function App() {
+    let [a, setA] = useState(1);
+
+    const OnClickHandler = () => {
+        setA(a += 1);
+        console.log(a);
+    };
+
+    const OnClickHandlerDoZero = () => {
+        setA(a= 0);
+
+        console.log(a);
+    };
     return (
-        <div className="App">
-            <Button title={'MyYouTobeChanel1'} callBack={() => ButtonFoo('Vasya', 21, ' live in minsk')}/>
-            <Button title={'MyYouTobeChanel2'} callBack={() => ButtonFoo('Ivan', 22, '')}/>
-            <Button title={'stupid'} callBack={() => console.log('I am stupid button')}/>
-            <button onClick={() => console.log('I am stupid button')}>stupidButton</button>
+        <div>
+            <h1>{a}</h1>
+            <button onClick={OnClickHandler}>number+1</button>
+            <button onClick={OnClickHandlerDoZero}>0</button>
         </div>
     );
 }
